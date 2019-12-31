295dea46943ba4c2176c9b28a0f6168d1d1fb31d
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2019-12-30 17:41:00 -0800

    added a seperate variable file

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from time import ctime
+from the_variables import token
 
 client = commands.Bot(command_prefix = '.')
 
@@ -10,9 +11,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print('Message from {0.author} in #{0.channel}: {0.content}'.format(message))
     iasf = ctime() #idk why its iasf
+    print('Message from [{0.author}] in [#{0.channel}] on [{1}]: {0.content}'.format(message, iasf))
     with open('log.txt','a') as f:
         f.write('Message from [{0.author}] in [#{0.channel}] on [{1}]: {0.content}\n'.format(message, iasf))
 
-client.run('your token here')
+client.run(token)
